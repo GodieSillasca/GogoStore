fe215e7f7748de7c196053f34ae7c7b038a4da9c
GodieSillasca <dcasillasgreco@gmail.com>
2020-02-23 22:34:56 -0600

    Subida de la Tienda Inicial

---
+++ js/app.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, options);
+});
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true
+  });
+
+  $(document).ready(function() {
+    M.updateTextFields();
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, options);
+  });
+
+
+
